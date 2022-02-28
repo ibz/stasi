ceaaa730ed1d993fce582a6517b1d282512ad24d
Ioan Bizău <i@ibz.me>
2022-02-28 17:54:28 +0200

    Ability to group datasources in graphs.

--- scripts/graph.sh
+++ scripts/graph.sh
@@ -7,4 +7,16 @@ else
   exit 1
 fi
 
-TZ=$GRAPH_TZ python3 /scripts/rrd_graph.py data images
+html="<html><body>"
+
+i=0
+for graph in "${GRAPHS[@]}"; do
+  for image_file in `TZ=$GRAPH_TZ python3 /scripts/rrd_graph.py data images $i $graph`; do
+    html+="<img src='$image_file' />"
+  done
+  let i++
+done
+
+html+="</body></html>"
+
+echo $html > images/index.html
--- scripts/rrd_graph.py
+++ scripts/rrd_graph.py
@@ -9,11 +9,14 @@ COLORS = ['#0b84a5', '#f6c85f', '#6f4e7c', '#9dd866', '#ca472f', '#ffa056', '#8d
 
 INTERVALS = ['1day', '1week', '1month', '6months']
 
-def rrd_graph(datadir, htmldir, field):
+def rrd_graph(datadir, htmldir, graph_index, graph):
+    field, sources = graph.split(':')
+    sources = sources.split('+')
+
     sensors = []
     for sensor in get_sensors(datadir):
         fields, first_ts = inspect_sensor(datadir, sensor)
-        if field in fields:
+        if field in fields and sensor in sources:
             sensors.append(sensor)
 
     defs = ['DEF:%s_%d=%s:%s:AVERAGE' % (field, i, os.path.join(datadir, '%s.rrd' % sensor), field) for i, sensor in enumerate(sensors, 1)]
@@ -22,18 +25,12 @@ def rrd_graph(datadir, htmldir, field):
     generated_files = []
 
     for interval in INTERVALS:
-        filename = '%s_%s.png' % (field, interval)
+        filename = '%s_%s_%s.png' % (field, graph_index, interval)
         rrdtool.graph(os.path.join(htmldir, filename),
                 '-w', '800', '-h', '120', '-a', 'PNG', '--slope-mode',
                 '--start', '-%s' % interval,
                 *(defs + lines))
-        generated_files.append(filename)
-
-    with open(os.path.join(htmldir, '%s.html' % field), 'w') as html_file:
-        html_file.write("<html><body>\n")
-        for filename in generated_files:
-            html_file.write("<img src='%s' />\n" % filename)
-        html_file.write("</body></html>\n")
+        print(filename)
 
 if __name__ == '__main__':
-    rrd_graph(sys.argv[1], sys.argv[2], 'temp')
+    rrd_graph(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
